f5659a8b06a608bdca183dae60c0d0694bbdad15
mehdismd <almahdi.mazari@ugent.be>
2022-12-21 14:20:37 +0100

    cakb

--- components/specificComponents/Blogposts/Blogposts.js
+++ components/specificComponents/Blogposts/Blogposts.js
@@ -1,16 +1,54 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
+// export default class Blogposts extends Component {
+
+//     constructor(props) {
+//        super(props);
+//    }
+
+//    render() {
+//        return (
+//            <div>
+//                Blogposts:{this.props.blok.title}</div>
+//        );
+
+//    }
+// }
+
+import React, { Component } from "react";
+import css from "./Blogposts.module.scss";
+import Headermenu from "../../genericComponents/Headermenu/Headermenu";
+import Hero from "../../genericComponents/Hero/Hero";
+import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
 
 export default class Blogposts extends Component {
-    constructor(props) {
 
-        super(props);
+	constructor(props) {
+		super(props);
+	}
 
-    }
-    render() {
-        return (
-            <div>
-                Blogposts:{this.props.blok.title}</div>
-        );
-    }
+	render() {
+		return (
+			<div {...storyblokEditable(this.props.blok)}>
+				<Headermenu blok={this.props.menu.content}></Headermenu>
+				<main>
+					<Hero blok={this.props.blok} contentTypeTag="blogposts" />
+					<div className={css["blogposts__main-content"]}>
+						<div id="blogposts__short-description" key="blogposts__short-description" className={css["blogposts__short-description"]}>
+							<section className={css["rich-text-section--with-navigator"]}>
+								<h2 className={css["rich-text-section__title"]}>Posts about us</h2>
+								<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.description })}</div>
+							</section>
+						</div>
+                    </div>
+
+                    {this.props.blok.bottombloks && this.props.blok.bottombloks.map((nestedBlok) => (
+						<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+					))}
+				</main>
+			</div>
+		);
+
+	}
 }
--- components/specificComponents/Location/Location.js
+++ components/specificComponents/Location/Location.js
@@ -1,17 +1,54 @@
+// import React, { Component } from "react";
 
+// export default class Location extends Component {
+
+//     constructor(props) {
+//        super(props);
+//    }
+
+//    render() {
+//        return (
+//            <div>
+//                Location:{this.props.blok.title}</div>
+//        );
+
+//    }
+// }
 
 import React, { Component } from "react";
+import css from "./Location.module.scss";
+import Headermenu from "../../genericComponents/Headermenu/Headermenu";
+import Hero from "../../genericComponents/Hero/Hero";
+import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
 
 export default class Location extends Component {
-    constructor(props) {
 
-        super(props);
+	constructor(props) {
+		super(props);
+	}
 
-    }
-    render() {
-        return (
-            <div>
-                Location:{this.props.blok.title}</div>
-        );
-    }
+	render() {
+		return (
+			<div {...storyblokEditable(this.props.blok)}>
+				<Headermenu blok={this.props.menu.content}></Headermenu>
+				<main>
+					<Hero blok={this.props.blok} contentTypeTag="location" />
+					<div className={css["location__main-content"]}>
+						<div id="location__short-description" key="location__short-description" className={css["location__short-description"]}>
+							<section className={css["rich-text-section--with-navigator"]}>
+								<h2 className={css["rich-text-section__title"]}>Our Location</h2>
+								<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.description })}</div>
+							</section>
+						</div>
+                    </div>
+
+                    {this.props.blok.bottombloks && this.props.blok.bottombloks.map((nestedBlok) => (
+						<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+					))}
+				</main>
+			</div>
+		);
+
+	}
 }
